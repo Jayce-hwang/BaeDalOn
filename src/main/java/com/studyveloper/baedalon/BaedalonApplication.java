2a4baaaaf1741f11a970fa9f7d64371e6d549a31
DONGHOON JEON <dhjeon1993@gmail.com>
2020-08-10 21:19:02 +0900

    [SETTING] 재업

---
+++ src/main/java/com/studyveloper/baedalon/BaedalonApplication.java
@@ -0,0 +1,13 @@
+package com.studyveloper.baedalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaedalonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaedalonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studyveloper/baedalon/BaedalonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyveloper.baedalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaedalonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
