a0dfc302e620e0128a9751baed0c48b8e86a939d
KimJunSung <kjs775@nate.com>
2020-08-30 12:22:38 +0900

    [FEATURE, ShopStatus.java] - Open Clode 상태값 추가

--- src/main/java/com/studyveloper/baedalon/shop/ShopStatus.java
+++ src/main/java/com/studyveloper/baedalon/shop/ShopStatus.java
@@ -1,4 +1,15 @@
 package com.studyveloper.baedalon.shop;
 
+import lombok.Getter;
+
+@Getter
 public enum ShopStatus {
+    CLOSE("close", false), OPEN("open", true);
+    private String statusName;
+    private boolean statusValue;
+
+    ShopStatus(String statusName, boolean statusValue) {
+        this.statusName = statusName;
+        this.statusValue = statusValue;
+    }
 }
