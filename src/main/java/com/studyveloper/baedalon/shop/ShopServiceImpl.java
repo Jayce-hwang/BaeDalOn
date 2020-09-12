c5ef620fb81a99a4d446e7a3d1be00eaacfa1179
KimJunSung <kjs775@nate.com>
2020-09-13 00:18:34 +0900

    [FEATURE/shopService] - 메소드마다 ownerId 삭제, 한 업주의 다중 가게생성 방지

--- src/main/java/com/studyveloper/baedalon/shop/ShopServiceImpl.java
+++ src/main/java/com/studyveloper/baedalon/shop/ShopServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.Entity;
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
@@ -24,37 +25,36 @@ public class ShopServiceImpl implements ShopService {
     private final OwnerRepository ownerRepository;
 
     @Override
-    public Long createShop(ShopCreateDTO shopCreateDTO, Long ownerId) {
-//        Owner owner = ownerRepository.findById(ownerId).orElseThrow(EntityNotFoundException::new);
-        Shop shop= Shop.builder()
-                .address(shopCreateDTO.getAddress())
-                .description(shopCreateDTO.getDescription())
-                .name(shopCreateDTO.getName())
-                .owner(shopCreateDTO.getOwner())
-                .phone(shopCreateDTO.getPhone())
-                .status(shopCreateDTO.getStatus())
-                .build();
-        shop = shopRepository.save(shop);
+    public Long createShop(ShopCreateDTO shopCreateDTO) {
+        // TODO :: 머지 후 변경
+        Owner owner = ownerRepository.findById(shopCreateDTO.getOwnerId()).orElseThrow(EntityNotFoundException::new);
+        Shop shop;
+        try{
+            ShopDetails shopDetails = searchShopByOwnerId(owner.getId());
+            return null;
+            // TODO :: 가게를 이미 생성한 업주인 경우 어캐? searchShopByOwner를 만들긴 했는데 여기서 catch에서 가게 생성하는게 맞는건가? catch에서 하니까 좀..
+        }catch(EntityNotFoundException e){
+            shop= Shop.builder()
+                    .address(shopCreateDTO.getAddress())
+                    .description(shopCreateDTO.getDescription())
+                    .name(shopCreateDTO.getName())
+                    .owner(owner)
+                    .phone(shopCreateDTO.getPhone())
+                    .build();
+            shop = shopRepository.save(shop);
+        }
         return shop.getId();
     }
     @Override
-    public void openShop(Long ownerId, Long shopId) {
+    public void openShop( Long shopId) {
         Shop shop = shopRepository.findById(shopId).orElseThrow(EntityNotFoundException::new);
-        if(shop.getOwner().getId() == ownerId){
-            shop.open();
-        }else{
-            //throws
-        }
+        shop.open();
     }
 
     @Override
-    public void closeShop(Long ownerId, Long shopId) {
+    public void closeShop( Long shopId) {
         Shop shop = shopRepository.findById(shopId).orElseThrow(EntityNotFoundException::new);
-        if(shop.getOwner().getId() == ownerId){
-            shop.close();
-        }else{
-            //throws
-        }
+        shop.close();
     }
 
     @Override
@@ -64,13 +64,10 @@ public class ShopServiceImpl implements ShopService {
     }
 
     @Override
-    public void deleteShop(Long ownerId, Long shopId) {
+    public void deleteShop( Long shopId) {
+        // TODO :: 가게도 삭제상태로? 아니면 바로 삭제?
         Shop shop = shopRepository.findById(shopId).orElseThrow(EntityNotFoundException::new);
-        if(shop.getOwner().getId() == ownerId){
-            shopRepository.delete(shop);
-        }else{
-            //throws
-        }
+        shopRepository.delete(shop);
     }
 
     @Override
@@ -81,13 +78,28 @@ public class ShopServiceImpl implements ShopService {
                 .address(shop.getAddress())
                 .description(shop.getDescription())
                 .name(shop.getName())
-                .owner(shop.getOwner())
+                .ownerId(shop.getOwner().getId())
                 .phone(shop.getPhone())
                 .status(shop.getStatus())
                 .build();
         return shopDetails;
     }
 
+    public ShopDetails searchShopByOwnerId(Long ownerId){
+        Shop shop = shopRepository.findByOwnerId(ownerId).orElseThrow(EntityNotFoundException::new);
+        ShopDetails shopDetails = ShopDetails.builder().
+                    id(shop.getId()).
+                    ownerId(ownerId).
+                    name(shop.getName()).
+                    address(shop.getAddress()).
+                    status(shop.getStatus()).
+                    phone(shop.getPhone()).
+                    description(shop.getDescription()).
+                    createdAt(shop.getCreatedAt()).
+                    modifiedAt(shop.getModifiedAt()).
+                    build();
+        return shopDetails;
+    }
     @Override
     public List<ShopDetails> searchShop(Pageable pageable, SearchCondition searchCondition) {
         //?
