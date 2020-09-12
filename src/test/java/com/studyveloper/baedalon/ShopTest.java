13837f8323fc6d5e024fbd0cb0cf54cf32242358
KimJunSung <kjs775@nate.com>
2020-09-13 00:21:43 +0900

    [FEATURE/shopService] - 가게 생성, 한 업주당 1가게 조건 실패, 가게 정보 수정, 가게 삭제, 가게 삭제 실패

--- src/test/java/com/studyveloper/baedalon/ShopTest.java
+++ src/test/java/com/studyveloper/baedalon/ShopTest.java
@@ -1,33 +1,23 @@
 package com.studyveloper.baedalon;
 
+import com.studyveloper.baedalon.builder.ShopBuilder;
 import com.studyveloper.baedalon.repository.OwnerRepository;
-import com.studyveloper.baedalon.shop.Shop;
 import com.studyveloper.baedalon.shop.ShopService;
 import com.studyveloper.baedalon.shop.ShopStatus;
 import com.studyveloper.baedalon.shop.dto.ShopCreateDTO;
 import com.studyveloper.baedalon.shop.dto.ShopDetails;
 import com.studyveloper.baedalon.shop.dto.ShopEditDTO;
 import com.studyveloper.baedalon.user.Owner;
-import com.studyveloper.baedalon.user.OwnerStatus;
-import lombok.RequiredArgsConstructor;
-import org.hamcrest.CoreMatchers;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityNotFoundException;
-import java.time.LocalDateTime;
-import java.util.Optional;
-
-import static org.junit.Assert.assertThat;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 @Transactional
 public class ShopTest {
     @Autowired
@@ -36,79 +26,62 @@ public class ShopTest {
     private OwnerRepository ownerRepository;
 
 
-    //가게 성공 등록 테스트
-    //DisplayName 어캐 하기로했지?
+    @DisplayName("가게생성 성공")
     @Test
-    public void 가게생성(){
-        Owner owner = Owner.builder()
-                .id((long)0)
-                .build();
+    public void create_shop_success(){
+        Owner owner = new Owner((long)1);//임시로 생성자 하나 만듬
         owner = ownerRepository.save(owner);
-        ShopCreateDTO shopCreateDTO = ShopCreateDTO.builder()
-                .address("가게 주소")
-                .description("가게 설명")
-                .name("가게 이름")
-                .owner(owner)
-                .phone("가게 번호")
-                .status(ShopStatus.OPEN)
-                .build();
-        Long shopId = shopService.createShop(shopCreateDTO, owner.getId());
-        Assert.isTrue(shopId==owner.getId(), "가게 생성 성공");
+        ShopCreateDTO shopCreateDTO = ShopBuilder.shopCreateDTODummyBuild();
+        Long shopId = shopService.createShop(shopCreateDTO);
+        Assertions.assertThat(shopId).isNotNull();
     }
 
+    @DisplayName("가게생성 업주당 1가게 조건 실패")
     @Test
-    public void 가게정보수정(){
-        Owner owner = Owner.builder()
-                .id((long)0)
-                .build();
+    public void create_shop_fail(){
+        Owner owner = new Owner((long)1);
         owner = ownerRepository.save(owner);
-        ShopCreateDTO shopCreateDTO = ShopCreateDTO.builder()
-                .address("가게 주소")
-                .description("가게 설명")
-                .name("가게 이름")
-                .owner(owner)
-                .phone("가게 번호")
-                .status(ShopStatus.OPEN)
-                .build();
-        Long shopId = shopService.createShop(shopCreateDTO, owner.getId());
-        ShopDetails shopDetails = shopService.findShop(shopId);
-        ShopEditDTO shopEditDTO = ShopEditDTO.builder()
-                .id(shopId)
-                .address("가게 정보 수정")
-                .description("가게 설명수정")
-                .name("가게 이름 수정")
-                .owner(owner)
-                .phone("가게 번호 수정")
-                .status(ShopStatus.CLOSE)
-                .build();
+        ShopCreateDTO shopCreateDTO = ShopBuilder.shopCreateDTODummyBuild();
+        Long shopId = shopService.createShop(shopCreateDTO);
+        Assertions.assertThat(shopId).isNotNull();
+
+        shopId = shopService.createShop(shopCreateDTO);
+        Assertions.assertThat(shopId).isNull();
+    }
+
+    @DisplayName("가게 정보수정 성공")
+    @Test
+    public void shop_edit_success(){
+        Owner owner = new Owner((long)1);
+        owner = ownerRepository.save(owner);
+        ShopCreateDTO shopCreateDTO = ShopBuilder.shopCreateDTODummyBuild();
+        Long shopId = shopService.createShop(shopCreateDTO);
+        Assertions.assertThat(shopId).isNotNull();
+
+        ShopEditDTO shopEditDTO = ShopBuilder.shopEditDTONameBiulder("가게이름수정");
         shopService.editShop(shopId, shopEditDTO);
-        ShopDetails shopDetails1 = shopService.findShop(shopId);
-        Assert.isTrue(!shopDetails.equals(shopDetails1), "수정 성공!");
+        ShopDetails shopDetails = shopService.findShop(shopId);
+
+        // TODO :: 왜 CreationTimestamp, UpdateTimestamp 적용 안 됨?
+        System.out.println(shopDetails);
+        Assertions.assertThat(shopDetails.getModifiedAt()).isNotNull();
     }
 
+    @DisplayName("가게 삭제 성공")
     @Test
-    public void 가게삭제(){
-        Owner owner = Owner.builder()
-                .id((long)0)
-                .build();
-        owner = ownerRepository.save(owner);
-        ShopCreateDTO shopCreateDTO = ShopCreateDTO.builder()
-                .address("가게 주소")
-                .description("가게 설명")
-                .name("가게 이름")
-                .owner(owner)
-                .phone("가게 번호")
-                .status(ShopStatus.OPEN)
-                .build();
-        Long shopId = shopService.createShop(shopCreateDTO, owner.getId());
+    public void shop_delete_success(){
+        ShopCreateDTO shopCreateDTO = ShopBuilder.shopCreateDTODummyBuild();
+        Long shopId = shopService.createShop(shopCreateDTO);
         ShopDetails shopDetails = shopService.findShop(shopId);
-        shopService.deleteShop(owner.getId(), shopDetails.getId());
-        try{
-            shopService.findShop(shopDetails.getId());
-            Assert.isNull(null, "삭제실패");
-        }catch(EntityNotFoundException e){
-            Assert.notNull(null, "삭제완료");
-        }
+        shopService.deleteShop(shopDetails.getId());
+    }
+
+    @DisplayName("없는 shopId로 삭제를 요청한 경우 실패")
+    @Test
+    public void shop_delete_fail(){
+        Long shopId = (long)10;
+        ShopDetails shopDetails = shopService.findShop(shopId);
+        shopService.deleteShop(shopDetails.getId());
     }
 }
 
